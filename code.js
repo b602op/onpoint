26bc21799b0e02057e4ef802ba22906650940481
unknown <@15ISPOLATOV.tservicegroup.ru>
2019-05-12 03:47:42 +0300

    add arrow

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 const wrapper = document.querySelector('.container');
-const schemeOne = document.querySelector('.box__scheme_1'); //delete
-const schemeTwo = document.querySelector('.box__scheme_2'); //delete
-const schemeThree = document.querySelector('.box__scheme_3'); //delete
-const blizzard = document.querySelector('.horizontal_scroll_bar'); //delete
+const schemeOne = document.querySelector('.box__scheme_1'); 
+const schemeTwo = document.querySelector('.box__scheme_2'); 
+const schemeThree = document.querySelector('.box__scheme_3'); 
+const blizzard = document.querySelector('.horizontal_scroll_bar'); 
 const iceberg = document.querySelector('.hs_scale_slider_click');
 const boxSchemesContainer = document.querySelector('.box_schemes__container');
 const shadowScroll = document.querySelector('.hor_scroll_scale_shadow');
@@ -10,15 +10,14 @@ const shadowScroll = document.querySelector('.hor_scroll_scale_shadow');
 let isAnimating = false;
 let yOffset;
 
-let startWorck = false; //delete
-let xOffset; //delete
+let startWorck = false; 
+let xOffset; 
 
-//------------------delete--------------------
+//--------------------------------------
 blizzard.ontouchmove = function(e) { //823 173 
 	let rightCorderCoord = 823;
 
 	if ((e.changedTouches[0].clientX > rightCorderCoord/3 && e.changedTouches[0].clientX < rightCorderCoord/2) || (e.changedTouches[0].clientX >rightCorderCoord/1.5 && e.changedTouches[0].clientX < rightCorderCoord-60)) {
-		console.log(e.changedTouches[0].clientX ,"мышка");
 		iceberg.style.right = rightCorderCoord - e.changedTouches[0].clientX + 'px';
 		shadowScroll.style.width = 800 - e.changedTouches[0].clientX + 'px';
 	} else if (e.changedTouches[0].clientX <= rightCorderCoord/2.1) {
@@ -63,7 +62,7 @@ document.body.ontouchend = function(e) {
 
 	if (endYOffset < yOffset) {
 		if (wrapper.classList.contains('first')) {
-		  wrapper.classList.add('second');
+	  		wrapper.classList.add('second');
 		} else {
 			wrapper.classList.add('first');
 		}
@@ -92,36 +91,3 @@ document.body.ontouchend = function(e) {
 function addCls(el) {
 	el.classList.add('yellow_dot');
 }
-
-
-//для колесика
-/*    document.body.onmousewheel = function(e) {
-if (isAnimating && !wrapper.classList.contains('second')) {
-return false;
-}
-
-isAnimating = true;
-setTimeout(() => {
-isAnimating = false;
-}, 500);
-
-if (e.deltaY > 0) {
-if (wrapper.classList.contains('first')) {
-  wrapper.classList.add('second');
-}
-
-wrapper.classList.add('first');
-}
-
-if (e.deltaY < 0) {
-if (wrapper.classList.contains('second')) {
-  return wrapper.classList.remove('second');
-}
-
-if (wrapper.classList.contains('first')) {
-  wrapper.classList.remove('first');
-}
-}
-}*/
-
-
